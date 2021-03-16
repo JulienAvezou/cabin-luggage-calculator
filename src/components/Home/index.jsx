d49aeedc895c6d330c7e51a9c538a3caa5114ad4
Julien Avezou <julien.avezou@gmail.com>
2021-03-16 15:15:42 +0100

    :sparkles: updating inventory and updating selected items

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -3,10 +3,13 @@ import Select from 'react-select';
 import { airlines } from './data';
 import axios from 'axios'
 import Loader from "react-loader-spinner";
+import './index.css';
 
 const Home = () => {
+  // state
   const [selectedAirline, setSelectedAirline] = useState(0);
   const [inventory, setInventory] = useState([]);
+  const [selectedItem, setSelectedItem] = useState([]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -29,6 +32,20 @@ const Home = () => {
     setSelectedAirline(value.weight);
   };
 
+  const handleClickAddItem = (item) => {
+    const newInventory = [...inventory];
+    const filteredInventory = newInventory.filter(el => el.name !== item.name);
+    setInventory(filteredInventory);
+    setSelectedItem((prevState) => [...prevState, item]);
+  };
+
+  const handleClickRemoveItem = (item) => {
+    const selectedInventory = [...selectedItem];
+    const filteredSelectedInventory = selectedInventory.filter(el => el.name !== item.name);
+    setSelectedItem(filteredSelectedInventory);
+    setInventory((prevState) => [...prevState, item]);
+  };
+
   const options = airlines.map((airline) => {
     return ({ value: airline.id, label: airline.name, weight: airline.weight })
   })
@@ -44,7 +61,7 @@ const Home = () => {
         />    
       </div>
       <div style={{marginTop: "10px", width: "650px", display: "flex"}}>
-        <div style={{marginRight: "5px", width: "300px", height: "200px", border: "1px solid black"}}>
+        <div style={{marginRight: "5px", width: "300px", height: "250px", border: "1px solid black"}}>
           {loading
             ? <Loader
                 type="TailSpin"
@@ -54,23 +71,55 @@ const Home = () => {
                 timeout={4000} 
               />
             : (
+              <div>
+              <div>Inventory</div>
               <ul>
               {inventory.map((item) => (
-                <li style={{display: "flex", justifyContent: "space-between"}}>
+                <li key={item.name} style={{display: "flex", justifyContent: "space-between"}}>
                   <div>
                     {item.name}
                   </div>
-                  <div onHover={() => console.log('test')}>
-                    {item.weight}
+                  <div>
+                    <div className="hidden">{item.weight}</div>
+                    <div
+                      className="show add"
+                      onClick={() => handleClickAddItem(item)}
+                    >Add</div>
                   </div>
                 </li>
               ))}
               </ul>
+              </div>
             )
           }
         </div>
-        <div style={{width: "100px", height: "300px", border: "1px solid black"}}>
-          body
+        <div style={{width: "300px", height: "300px", border: "1px solid black"}}>
+          <div>Selected</div>
+          {selectedItem.length
+          ? (
+            <>
+              <ul>
+                {selectedItem.map((item) => (
+                  <li key={item.name} style={{display: "flex", justifyContent: "space-between"}}>
+                    <div>
+                      {item.name}
+                    </div>
+                    <div className="hidden">{item.weight}</div>
+                    <div
+                      className="show add"
+                      onClick={() => handleClickRemoveItem(item)}
+                    >
+                      Remove
+                    </div>
+                  </li>  
+                ))}
+              </ul>
+              <div>
+              
+              </div>
+            </>
+          )
+           : null}
         </div>
       </div>
     </>
